e4e1f87648b589e1585093749f062bc09d556c3c
juanvazbe@gmail.com <33Juan>
2022-04-29 19:43:33 +0200

    Updateg

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Panel') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('¡Estás logeado!') }}
+
+                    <form action="http://127.0.0.1:8000/posts">
+                        <input type="submit" value="Go to Posts" />
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
